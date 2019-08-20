673243e1ed33865d87070b4b7e31aa094db88d28
Shaiell <jcl.ribeiro@hotmail.com>
2019-08-20 20:33:46 -0300

    Initial commit

---
+++ app/src/main/java/br/ici/treinamento/nextcat/MainActivity.java
@@ -0,0 +1,22 @@
+package br.ici.treinamento.nextcat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void novoGato(View view){
+
+        ImageView gato = (ImageView) findViewById(R.id.imageViewcat1);
+        gato.setImageResource(R.drawable.cat2);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
